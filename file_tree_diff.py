23936969199c6ae0b4c16d73c4c856cddca2c125
Chill Magic <hanlengmowang@hotmail.com>
2023-12-19 02:16:16 +0800

    Show dir move status instead of files in dir

--- file_tree_diff.py
+++ file_tree_diff.py
@@ -1,6 +1,7 @@
-import filecmp
-from pathlib import Path
+from . import new_filecmp as filecmp
+from pathlib import Path, PurePath
 from typing import List, Optional
+from collections import OrderedDict
 
 
 DumpScan = False
@@ -31,7 +32,7 @@ def analyze_dir_diffs(base_dir1: Path, base_dir2: Path, filter_list: Optional[Li
     return record_dir1, record_dir2, record_diff
 
 
-def do_diff(old_dir: Path, new_dir: Path, filter_list: Optional[List[str]] = None, extra_sig_map: Optional[set] = None):
+def do_diff(old_dir: Path, new_dir: Path, filter_list: Optional[List[str]] = None, extra_sig_map: Optional[dict] = None):
     record_old_dir, record_new_dir, record_diff = analyze_dir_diffs(old_dir, new_dir, filter_list)
 
     def get_sig_map(base_dir: Path, record_list):
@@ -62,6 +63,7 @@ def do_diff(old_dir: Path, new_dir: Path, filter_list: Optional[List[str]] = Non
     old_sig_map = get_sig_map(old_dir, record_old_dir)
     new_sig_map = get_sig_map(new_dir, record_new_dir)
     move_record_list = []
+    move_record_map = {}
     old_moved = set()
     new_moved = set()
     for sig, paths in old_sig_map.items():
@@ -73,11 +75,48 @@ def do_diff(old_dir: Path, new_dir: Path, filter_list: Optional[List[str]] = Non
                 new_moved.add(new_item)
 
             move_record_list.append((old, new))
-        if sig in extra_sig_map:
+            for old_item in old:
+                move_record_map[old_item] = new
+        if extra_sig_map and sig in extra_sig_map:
             old, extra = paths, extra_sig_map[sig]
             for old_item in old:
                 old_moved.add(old_item)
             move_record_list.append((old, extra))
+            for old_item in old:
+                move_record_map[old_item] = extra
+
+    filter_old_files = set()
+    for (dir, file_list) in record_old_dir:
+        dir_path = old_dir / dir
+        for file in file_list:
+            path = dir_path / file
+            if path.is_dir():
+                files = []
+                def _traverse(p):
+                    if p.is_dir():
+                        for pp in p.iterdir():
+                            _traverse(pp)
+                    elif p.is_file():
+                        files.append(p)
+                _traverse(path)
+
+                move_to_dirs = OrderedDict()
+                will_filter_old_files = set()
+                for f in files:
+                    will_filter_old_files.add(f.relative_to(old_dir))
+                    current_dir_path_str = str(f.relative_to(path).parent)
+                    current_dir_path_str = '' if current_dir_path_str == '.' else current_dir_path_str
+                    if f.relative_to(old_dir) not in move_record_map:
+                        continue
+                    move_to_list = move_record_map[f.relative_to(old_dir)]
+                    if len(move_to_list) == 1:
+                        move_to_path_str = str(move_to_list[0].parent)
+                        if move_to_path_str.endswith(current_dir_path_str):
+                            move_to_dirs[PurePath(move_to_path_str[:len(move_to_path_str)-len(current_dir_path_str)])] = True
+                if move_to_dirs:
+                    old_moved.add(Path(dir) / file)
+                    move_record_list.append(([Path(dir) / file],list(move_to_dirs.keys())))
+                    filter_old_files.update(will_filter_old_files)
 
     print('-' * 100)
     print(f"Only in `{old_dir}`:")
@@ -91,6 +130,14 @@ def do_diff(old_dir: Path, new_dir: Path, filter_list: Optional[List[str]] = Non
     print('-' * 100)
     print(f'Moved:')
     for old, new in move_record_list:
+        if isinstance(old, list):
+            new_old = []
+            for old_item in old:
+                if old_item not in filter_old_files:
+                    new_old.append(old_item)
+            old = new_old
+        if not old:
+            continue
         if isinstance(old, list) and len(old) == 1:
             old = old[0]
         if isinstance(new, list) and len(new) == 1:
